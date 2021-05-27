220fa24de9f4279a6f26e7c139b7bb86b37472b7
mehaotian <490272692@qq.com>
2021-05-27 18:54:36 +0800

    fix: 修复不能热刷的问题

--- src/util/file.js
+++ src/util/file.js
@@ -2,13 +2,16 @@ import fs from 'fs'
 import path from 'path'
 
 export function readFile(fsPath) {
-	let content = {}
-	try {
-		// TODO 不确认是否会有问题 ，待测试
-		// content = content.replace(/\\\\u/, '\\u')
-		content = require(fsPath)
-	} catch (e) {
-		content = false
+	let content = fs.readFileSync(fsPath).toString()
+	if(content){
+		try {
+			// TODO 不确认是否会有问题 ，待测试
+			// content = content.replace(/\\\\u/, '\\u')
+			content = JSON.parse(content)
+		} catch (e) {
+			content = false
+		}
+		return content
 	}
-	return content
+	return false
 }
